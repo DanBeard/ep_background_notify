5a28ba699423374adb9b83dfbce676ebe182907e
Dan Beard <dryawgmoth@gmail.com>
2013-08-11 14:33:52 -0700

    added cross document messaging for html5 iframe support

--- static/js/client.js
+++ static/js/client.js
@@ -5,21 +5,29 @@
  * @description: Client library that will alert the user is something changed while they weren't looking at the pad
  * @requires: jQuery
  */
-exports.documentReady = function(){
-
+        exports.documentReady = function(){
         exports.is_active = true; //is this document the one that user is looking at?
         $(window).focus(function(){exports.is_active = true});
         $(window).blur(function(){exports.is_active = false});
         //add an audio tag to the body
-        $('body').append($("<audio id='ep_background_notify_sound' src='static/plugins/ep_background_notify/static/media/notify.wav' preload />"))
+        $('body').append($("<audio id='ep_background_notify_sound' src='/static/plugins/ep_background_notify/static/media/notify.wav' preload />"));
+
+        //if we're in an iframe, we need the parent window to be able to communicate focus and blur events to us
+        //use cross document messaging for that http://en.wikipedia.org/wiki/Cross-document_messaging
+        window.addEventListener('message', function(event){
+            if(event.data == 'true') exports.is_active = true;
+            else if(event.data == 'false') exports.is_active = false;
+        }, false);
 
 };
 
-exports.aceEditEvent = function(){
-
+exports.handleClientMessage_NEW_CHANGES = function(){
     if(exports.is_active) return []; // don't care if we're active
 
     //play a the audio
     $('#ep_background_notify_sound')[0].play();
 
-}
+};
+
+
+
